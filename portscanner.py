0cd42b522c3352be5a07a5075fdd8d6a76ff5e01
hawaiichan <kamayukanasafu@gmail.com>
2025-03-15 15:18:24 +0900

    portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,15 +1,19 @@
 import socket                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                           
+
+#ポートの範囲を設定
 max_port = 65535                                                                                                                                                                                                                           
-min_port = 1                                                                                                                                                                                                                               
+min_port = 1    
+
 print("IPアドレスを入れてください")                                                                                                                                                                                                        
 target_host = input(":")                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                           
+
+#ここでポートをスキャンする
 for port in range(min_port, max_port):                                                                                                                                                                                                     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                                                                                                                                               
     return_code = sock.connect_ex((target_host, port))                                                                                                                                                                                     
     sock.close()                                                                                                                                                                                                                           
-                                                                                                                                                                                                                                           
+
+#開いているポートを見つけたら0を返す
     if return_code == 0:                                                                                                                                                                                                                   
         print("Port %d open!" % (port))                                                                                                                                                                                                    
                                                                                                                                                                                                                                            
